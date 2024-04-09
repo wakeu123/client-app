6cc74bdfbbf54e2a2fae7135bfd4a84a189b42df
wakeu123 <georgeswakeu21@gmail.com>
2024-04-09 11:28:34 +0100

    u

--- src/app/features/components/search/search.component.ts
+++ src/app/features/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+
+  search = model<string>();
 
 }
