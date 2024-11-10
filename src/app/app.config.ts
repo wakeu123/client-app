c6d71e4e9792187ba0fbbfa93276525860de0ee1
wakeu123 <georgeswakeu21@gmail.com>
2024-11-10 21:43:31 +0100

    add toast component to home page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter, TitleStrategy, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { AngularSplitModule } from 'angular-split';
 import { provideHttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomTitleStrategy } from './shared/services/custom-title-strategy.service';
-import { NgxSpinnerModule } from 'ngx-spinner';
+import { provideRouter, TitleStrategy, withComponentInputBinding } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,6 +22,7 @@ export const appConfig: ApplicationConfig = {
       ReactiveFormsModule, 
       AngularSplitModule, 
       NgxSpinnerModule,
+      ToastModule
     )
   ]
 };
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,18 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerComponent } from 'ngx-spinner';
 import { UserService } from '../users/user.service';
 import { ListUserComponent } from '../users/list.component';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ListUserComponent, NgxSpinnerComponent],
+  imports: [ListUserComponent, NgxSpinnerComponent, ToastModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  providers: [UserService]
+  providers: [UserService, MessageService]
 })
 export class HomeComponent implements OnInit {
 
-    ngOnInit(): void {
-  
-    }
+  constructor(private messageService: MessageService) {}
+
+  show() {
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+  }
+
+  ngOnInit(): void {
+
+  }
 }
